718fa9d9a4c64d226dc82023d24dd63248d8b7b4
zachartay1994 <zacharytay1994@gmail.com>
2021-07-01 21:39:25 +0800

    abstract glfw init and imgui

---
+++ JZEngine/src/JZEngine/JZGL/JZ_GL.cpp
@@ -0,0 +1,78 @@
+/*	__FILE HEADER__
+*	File:	JZ_GL.cpp
+	Author: JZ
+	Date:	01/07/21
+	Brief:	Encapsulates all OpenGL initializations
+			and updates using GLFW and glad.
+*/
+
+#include <PCH.h>
+#include "JZ_GL.h"
+
+namespace JZEngine
+{
+	GLFW_Instance::GLFW_Instance(int width, int height)
+		:
+		window_width_(width),
+		window_height_(height)
+	{
+		Initialize();
+	}
+
+	GLFW_Instance::~GLFW_Instance()
+	{
+
+	}
+
+	void GLFW_Instance::FrameStart()
+	{
+		glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+	}
+
+	void GLFW_Instance::FrameEnd()
+	{
+		glfwPollEvents();
+		glfwSwapBuffers(window_);
+	}
+
+	bool GLFW_Instance::Active()
+	{
+		return !glfwWindowShouldClose(window_);
+	}
+
+	void GLFW_Instance::Initialize()
+	{
+		glfwInit();
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+		glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+		// create glfw window
+		window_ = glfwCreateWindow(window_width_, window_height_, "LearnOpenGL", NULL, NULL);
+		if (window_ == NULL)
+		{
+			std::cout << "Failed to create GLFW window" << std::endl;
+			glfwTerminate();
+		}
+		glfwMakeContextCurrent(window_);
+
+		// test glad is working
+		if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+		{
+			std::cout << "Failed to initialize GLAD" << std::endl;
+		}
+
+		// create opengl viewport
+		glViewport(0, 0, window_width_, window_height_);
+
+		// set resize window callback function
+		glfwSetFramebufferSizeCallback(window_, FramebufferSizeCallback);
+	}
+
+	void JZEngine::FramebufferSizeCallback(GLFWwindow* window, int width, int height)
+	{
+		UNREFERENCED_PARAMETER(window);
+		UNREFERENCED_PARAMETER(window);
+		glViewport(0, 0, width, height);
+	}
+}
