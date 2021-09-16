de56d096311f44dcf35e6c4aa6bc384cdb6c230f
zachartay1994 <zacharytay1994@gmail.com>
2021-09-17 01:27:46 +0800

    math v1

--- JZEngine/src/JZEngine/Application.cpp
+++ JZEngine/src/JZEngine/Application.cpp
@@ -43,8 +43,6 @@ namespace JZEngine
 		/*testsystem.createSound("testsound", "../JZEngine/Resources/LOST CIVILIZATION - NewAge MSCNEW2_41.wav");
 		testsystem.playSound("testsound", true, 0.4f);
 		testsystem.setChannelGroupVolume(1.0f,"main");*/
-
-		Math::AllMatrixTestCases();
 	}
 
 	void Application::Free()
