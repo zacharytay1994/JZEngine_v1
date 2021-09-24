3382cd809e44e6fb02d128e9759249b5447d17c5
chingyin-yu <asd86601987@hotmail.com>
2021-09-25 01:46:14 +0800

    Update Message.h

--- JZEngine/src/JZEngine/Message/Message.h
+++ JZEngine/src/JZEngine/Message/Message.h
@@ -1,1 +1,28 @@
 #pragma once
+
+namespace JZEngine
+{
+	namespace MessageID
+	{
+		enum MessageIDType
+		{
+			ToggleDebugInfo,
+			CharacterKey,
+			MouseButton,
+			PlaySound
+		};
+	}
+
+	class Message
+	{
+	public:
+		Message(MessageID::MessageIDType id) : messageID(id) {};
+		virtual ~Message() {};
+
+	private:
+		MessageID::MessageIDType messageID;
+	};
+
+
+
+}
