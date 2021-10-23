36cd25a225b8e851ce6ce6e4c4b0b3fcd6967851
chingyin-yu <asd86601987@hotmail.com>
2021-10-24 05:49:41 +0800

    Update CustomerState.cpp

--- JZEngine/src/JZEngine/FSM/CustomerState.cpp
+++ JZEngine/src/JZEngine/FSM/CustomerState.cpp
@@ -1,11 +1,13 @@
 #include "PCH.h"
 #include "State.h"
 #include "FiniteStateMachine.h"
-#include "../Input/DeltaTime.h"
+#include "../DebugTools/PerformanceData.h"
 #include <iostream>
 
 namespace JZEngine
 {
+	float dt = 1.0f / PerformanceData::app_fps_;
+
 	//Use class to make the enums strongly typed and the enums are scoped
 	//to prevent acciental misuse of constants incase there are similar names
 	enum class CustomerStateType
@@ -33,13 +35,47 @@ namespace JZEngine
 
 		void update()
 		{
-			if (DeltaTime::get_deltatime() > 4.0)
+			if (JZEngine::dt > 3.0f)
 			{
 				std::cout << "Done ordering, transition to waiting state!" << "\n";
 				mFsm.setCurrentState(CustomerStateType::WAITING);
 			}
 		}
 
+
+	private:
+		FiniteStateMachine<CustomerStateType>& mFsm;
+
+	};
+
+	class CustomerWaitingState : public State<CustomerStateType>
+	{
+	public:
+		CustomerWaitingState(FiniteStateMachine<CustomerStateType>& fsm) 
+			: State<CustomerStateType>(CustomerStateType::WAITING, "Waiting")
+			, mFsm{fsm} 
+		{
+		
+		}
+
+		void enter()
+		{
+			std::cout << "Customer state: WAITING" << "\n";
+		}
+
+		void update()
+		{
+			if (JZEngine::dt > 6.0f)
+			{
+				std::cout << "Wait too long! ANGRY!" << "\n";
+				mFsm.setCurrentState(CustomerStateType::ANGRYLEAVE);
+			}
+			else if (JZEngine::dt <= 6.0f)
+			{
+				std::cout << "Completed order in time! Yay!" << "\n";
+				mFsm.setCurrentState(CustomerStateType::HAPPYLEAVE);
+			}
+		}
 
 	private:
 		FiniteStateMachine<CustomerStateType>& mFsm;
