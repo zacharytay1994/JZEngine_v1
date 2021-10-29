57ad3920aa52a4c83e6c61a3aa102a484c73d0dc
Zachary <zacharytay1994@gmail.com>
2021-10-29 11:23:10 +0800

    removed broken scenes and prefabs

--- JZEngine/src/JZEngine/GraphicRendering/TransformComponent.h
+++ JZEngine/src/JZEngine/GraphicRendering/TransformComponent.h
@@ -5,6 +5,7 @@ namespace JZEngine
 	struct Transform
 	{
 		Vec2f position_{ 0.0f, 0.0f };
+		Vec2f child_position_{ 0.0f, 0.0f };
 		float rotation_{ 0.0f };
 		Vec2f size_{ 50.0f, 50.0f };//physics system uses the size_
 		Vec2f scale_{ 1.0f,1.0f };
