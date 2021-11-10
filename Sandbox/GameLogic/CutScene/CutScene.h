94601c5d89fe9197feca617133fbdfe8b71b227d
chingyin-yu <asd86601987@hotmail.com>
2021-11-11 02:13:39 +0800

    save first

--- Sandbox/GameLogic/CutScene/CutScene.h
+++ Sandbox/GameLogic/CutScene/CutScene.h
@@ -1,1 +1,30 @@
 #pragma once
+
+#include "../ButtonLogic.h"
+
+enum class CutSceneState
+{
+	CutScene,
+	Skip
+};
+
+CutSceneState current_cut_scene_state = CutSceneState::CutScene;
+bool Message1st{false};
+
+void Flag1stMsg(bool flag)
+{
+	// active/inactive 1st msg
+	Scene().EntityFlagActive("1st_msg_bg", flag);
+	Scene().EntityFlagActive("1st_msg_emoji", flag);
+	Scene().EntityFlagActive("1st_msg_text", flag);
+}
+
+void CutSceneInit()
+{
+	current_cut_scene_state = CutSceneState::CutScene;
+	Flag1stMsg(false);
+}
+void CutSceneUpdate(float dt)
+{
+
+}
